dd86f937dc3b96f67dc8a07810c2710a1c4d421d
Reid Paape <wrpaape@gmail.com>
2015-05-13 11:35:50 -0500

    added array rails_class containing instances of Student class

--- student.rb
+++ student.rb
@@ -17,3 +17,25 @@ class Student
     "learned"
   end
 end
+
+student1 = Student.new("Reid","Ruby on Rails Engineering",3.0)
+student2 = Student.new("Bob","Ruby on Rails Engineering",2.0)
+student3 = Student.new("Tim","Ruby on Rails Engineering",1.0)
+student4 = Student.new("Sally","Ruby on Rails Engineering",3.5)
+student5 = Student.new("Erica","Ruby on Rails Engineering",2.5)
+student6 = Student.new("Joe","Ruby on Rails Engineering",1.5)
+student7 = Student.new("Seamus","Ruby on Rails Engineering",3.1)
+student8 = Student.new("Julie","Ruby on Rails Engineering",2.1)
+student9 = Student.new("Tom","Ruby on Rails Engineering",1.1)
+
+
+rails_class = [student1, \
+               student2, \
+               student3, \
+               student4, \
+               student5, \
+               student6, \
+               student7, \
+               student8, \
+               student9]
+
